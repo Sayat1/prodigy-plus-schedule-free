005e15bc3eca183313c973809f049bb739d3e42e
Logan Booker <loganbooker@gmail.com>
2025-05-02 00:50:49 +1000

    More tweaks.

--- prodigyplus/core_optimiser.py
+++ prodigyplus/core_optimiser.py
@@ -304,10 +304,7 @@ class CoreOptimiser(torch.optim.Optimizer):
         return state, needs_init
 
     def get_betas(self, group):
-        beta1, beta2 = group['betas']
-        if beta2 is None:
-            beta2 = 1 - (1 / group['k'])
-        return (beta1, beta2)
+        return group['betas']
 
     def get_beta3(self, group):
         beta3 = group['beta3']
@@ -388,7 +385,8 @@ class CoreOptimiser(torch.optim.Optimizer):
             prev_d_numerator, max_d_numerator = group['prev_d_numerator'], group['max_d_numerator']
 
             if k >= 2 and d_numerator >= max_d_numerator and d_numerator > 0 and prev_d_numerator > 0:
-                d_power = 2 * (d_coef ** 0.5)
+                beta = self.get_betas(group)[1] ** 2
+                d_power = (2 - (1 - beta ** k) ** 2) * (d_coef ** 0.5)
                 d_hat = min(2 ** 0.5, (d_numerator / prev_d_numerator) ** d_power)
                 d = max(d, d * d_hat)
         elif d_denom > 0:
@@ -456,8 +454,8 @@ class CoreOptimiser(torch.optim.Optimizer):
             x0_dot = torch.dot(sliced_grad, x0_minus)
 
             if group['use_speed']:
-                d_update = group['d0'] ** 0.75 # No effect on training; keeps scale at same magnitude as regular Prodigy
-                x0_dot /= x0_minus.norm().sqrt().clamp_min(1e-8)
+                d_update = group['d0'] ** 0.5 # No effect on training; keeps scale at same magnitude as regular Prodigy
+                x0_dot /= x0_minus.abs().sum().sqrt().clamp_min(1e-8)
             else:
                 d_update = (group['d'] ** 2) / (group['d0'] ** 0.5)
                 running_d_denom.add_(state['s'].mul_(beta3).add_(sliced_grad, alpha=d_update).abs().sum())
